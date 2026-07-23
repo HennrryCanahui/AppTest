ad5ffa777ff6557474c01e419a3f487b801465bd
HennrryCanahui <hennrrycanahuiholasoy@gmail.com>
2026-07-23 14:08:13 -0600

    se agrego la navegacion

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Screen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>¡Pantalla activa!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#020617' },
+    text: { color: '#f8fafc', fontSize: 18 },
+});
